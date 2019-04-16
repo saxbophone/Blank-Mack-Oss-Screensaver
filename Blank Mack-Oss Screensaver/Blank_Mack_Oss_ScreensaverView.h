79494d68aca43ae1f46bbb7bff6804b70c3c9e24
Joshua Saxby <joshua.a.saxby@gmail.com>
2019-04-16 01:43:10 +0100

    Initial Commit

---
+++ Blank Mack-Oss Screensaver/Blank_Mack_Oss_ScreensaverView.h
@@ -0,0 +1,13 @@
+//
+//  Blank_Mack_Oss_ScreensaverView.h
+//  Blank Mack-Oss Screensaver
+//
+//  Created by Joshua Saxby on 2019-04-16.
+//  Copyright © 2019 Joshua Saxby. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface Blank_Mack_Oss_ScreensaverView : ScreenSaverView
+
+@end
---
+++ Blank Mack-Oss Screensaver/Blank_Mack_Oss_ScreensaverView.m
@@ -0,0 +1,52 @@
+//
+//  Blank_Mack_Oss_ScreensaverView.m
+//  Blank Mack-Oss Screensaver
+//
+//  Created by Joshua Saxby on 2019-04-16.
+//  Copyright © 2019 Joshua Saxby. All rights reserved.
+//
+
+#import "Blank_Mack_Oss_ScreensaverView.h"
+
+@implementation Blank_Mack_Oss_ScreensaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
